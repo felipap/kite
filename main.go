f18223bedea5dbfa157394b060f06672353ded28
f03lipe <pires.a.felipe@gmail.com>
2015-02-24 22:08:07 -0300

    To EC2 server.

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+	if len(path) == 0 {
+		path = "everything"
+	}
+	fmt.Fprintf(w, "Hi there, I love %s!", path)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	port := 8080
+	fmt.Printf("Server listening on port %s\n", strconv.Itoa(port))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
